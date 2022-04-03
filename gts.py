6f2324fc16adecb156771e6e22e3ca465647e589
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-03 13:32:44 +0200

    some docs

--- gts.py
+++ gts.py
@@ -1,3 +1,25 @@
+# WIEK:
+#   Mlody - 0
+#   starczy - 1
+#   prestarczy - 2
+
+# WADA:
+#   Krot - 0
+#   Dal - 1
+
+# AST:
+#   Nie - 0
+#   Tak - 1
+
+# LZ:
+#   Norm - 0
+#   Zmn - 1
+
+# SOCZ:
+#   Brak - 0
+#   Miekkie - 1
+#   Twarde - 2
+
 import json
 f = open('input.json')
 data = json.load(f)
