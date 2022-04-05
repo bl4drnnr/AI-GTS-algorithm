63ed6d127c46eb4e2a6d31554bffd529bcdcb808
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-05 21:46:38 +0200

    new range

--- gts.py
+++ gts.py
@@ -10,24 +10,26 @@ attrsValues = []
 inputDataLen = len(data['inputdata'])
 firstRecord = data['inputdata'][0]
 
-for iterate in range(4):
-    totalQuantityOfAttr = 1
-    totalQuantityOfMismatchAttr = 1
-    for i in data['inputdata'][1:]:
+for x in range(inputDataLen):
+    for iterate in range(4):
+        totalQuantityOfAttr = 1
+        totalQuantityOfMismatchAttr = 1
+        for i in data['inputdata'][1:]:
 
-        if i[list(firstRecord)[iterate]] == firstRecord[list(firstRecord)[iterate]]:
-            totalQuantityOfAttr += 1
+            if i[list(firstRecord)[iterate]] == firstRecord[list(firstRecord)[iterate]]:
+                totalQuantityOfAttr += 1
 
-        if i[list(firstRecord)[iterate]] == firstRecord[list(firstRecord)[iterate]] \
-                and i['socz'] == firstRecord['socz']:
-            totalQuantityOfMismatchAttr += 1
+            if i[list(firstRecord)[iterate]] == firstRecord[list(firstRecord)[iterate]] \
+                    and i['socz'] == firstRecord['socz']:
+                totalQuantityOfMismatchAttr += 1
 
-    attrsValues.append({list(firstRecord)[iterate]: H(totalQuantityOfAttr, totalQuantityOfMismatchAttr, inputDataLen)})
+        attrsValues.append({list(firstRecord)[iterate]: H(totalQuantityOfAttr, totalQuantityOfMismatchAttr, inputDataLen)})
+
+print(attrsValues)
 
 for rule in attrsValues:
     for k, v in rule.items():
         if "rule" in v:
             createdRules.append("IF {rule} THEN {res}"
                                 .format(rule=getRule(k, str(firstRecord[k])), res=getRule('socz', str(firstRecord['socz']))))
-
 print(createdRules)
