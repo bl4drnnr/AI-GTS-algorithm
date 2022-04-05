e490d94a424ebd87b0f88e90ff862b582c81b93f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-05 21:50:46 +0200

    push as object

--- gts.py
+++ gts.py
@@ -10,10 +10,11 @@ attrsValues = []
 inputDataLen = len(data['inputdata'])
 firstRecord = data['inputdata'][0]
 
-for x in range(inputDataLen):
+for _ in range(inputDataLen):
     for iterate in range(4):
         totalQuantityOfAttr = 1
         totalQuantityOfMismatchAttr = 1
+        test = {}
         for i in data['inputdata'][1:]:
 
             if i[list(firstRecord)[iterate]] == firstRecord[list(firstRecord)[iterate]]:
@@ -23,13 +24,17 @@ for x in range(inputDataLen):
                     and i['socz'] == firstRecord['socz']:
                 totalQuantityOfMismatchAttr += 1
 
-        attrsValues.append({list(firstRecord)[iterate]: H(totalQuantityOfAttr, totalQuantityOfMismatchAttr, inputDataLen)})
+        test[list(firstRecord)[iterate]] = H(totalQuantityOfAttr, totalQuantityOfMismatchAttr, inputDataLen)
+        attrsValues.append(test)
+        test = {}
+        # attrsValues.append({list(firstRecord)[iterate]: H(totalQuantityOfAttr, totalQuantityOfMismatchAttr, inputDataLen)})
 
 print(attrsValues)
+# print(test)
 
 for rule in attrsValues:
     for k, v in rule.items():
         if "rule" in v:
             createdRules.append("IF {rule} THEN {res}"
                                 .format(rule=getRule(k, str(firstRecord[k])), res=getRule('socz', str(firstRecord['socz']))))
-print(createdRules)
+# print(createdRules)
