f863ebbaf16c220cc27425b021c9031ac8dccc83
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-06 18:26:06 +0200

    filtered data

--- gts.py
+++ gts.py
@@ -14,21 +14,28 @@ for x in range(inputDataLen):
         "ast": 0,
         "lz": 0,
     }
+    currentRecordDataFiltered = {
+        "wiek": 0,
+        "wada": 0,
+        "ast": 0,
+        "lz": 0,
+    }
 
     # Collecting information for one current record
     for y in data['inputdata']:
         for iterator in range(4):
             if y[list(currentRecord)[iterator]] == currentRecord[list(currentRecord)[iterator]]:
                 currentRecordData[list(currentRecord)[iterator]] += 1
+                currentRecordDataFiltered[list(currentRecord)[iterator]] += 1
 
     # Filter collected data to count values of G, A and H
     for z in data['inputdata']:
         if z['socz'] == currentRecord['socz']:
             for iterator in range(4):
                 if currentRecord[list(currentRecord)[iterator]] == z[list(currentRecord)[iterator]]:
-                    currentRecordData[list(currentRecord)[iterator]] -= 1
-    print(currentRecordData)
+                    currentRecordDataFiltered[list(currentRecord)[iterator]] -= 1
 
+    # Check if there is already generated rule
 
 # see docs
 # fix iterations
