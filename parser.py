d68d4a8657d15d0e9b48784b04eaaf752fb63667
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-07 19:55:21 +0200

    decision and conditional attributes

--- parser.py
+++ parser.py
@@ -2,6 +2,9 @@ import json
 
 f = open('input2.json')
 data = json.load(f)
+data = data['inputdata']
+decisionAttribute = list(data[0])[-1]
+conditionalAttributes = list(data[0])[0:len(list(data[0])) - 1]
 
 
 def parseInputData(inputData):
