e0ec23764c5fdd853cd34f911b2b47ae79c8e56c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-07 21:02:41 +0200

    working parser script

--- parser.py
+++ parser.py
@@ -3,9 +3,33 @@ import json
 f = open('input2.json')
 data = json.load(f)
 data = data['inputdata']
-decisionAttribute = list(data[0])[-1]
-conditionalAttributes = list(data[0])[0:len(list(data[0])) - 1]
+
+allPossibleAttributes = {}
+parsedInput = []
 
 
-def parseInputData(inputData):
-    return
+def parseInputData():
+    # Get all possible attributes and classes
+    for item in data:
+        for attr, value in item.items():
+            if allPossibleAttributes.get(attr) is None:
+                allPossibleAttributes[attr] = {value: 0}
+            else:
+                if allPossibleAttributes[attr].get(value) is None:
+                    allPossibleAttributes[attr][value] = 0
+    # Parse those attributes and classes and give values
+    for param in allPossibleAttributes:
+        i = 0
+        for attr, value in allPossibleAttributes[param].items():
+            allPossibleAttributes[param][attr] += i
+            i += 1
+    # Parse input data
+    for item in data:
+        for attr, value in item.items():
+            item[attr] = allPossibleAttributes[attr][value]
+
+
+parseInputData()
+print("allPossibleAttributes: " + str(allPossibleAttributes))
+for i in data:
+    print(i)
