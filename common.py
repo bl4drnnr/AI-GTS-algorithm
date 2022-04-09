9f5b6667e194042edd0cfb4618d10bd6b14bfae6
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-09 15:01:28 +0200

    generated new rules function

--- common.py
+++ common.py
@@ -1,3 +1,7 @@
+from parser import parseInputData
+data = parseInputData()
+
+
 def getRule(ruleName, ruleValue, allRules):
     return list(allRules[ruleName].keys())[list(allRules[ruleName].values()).index(ruleValue)]
 
@@ -11,7 +15,6 @@ def getTwoMaxValues(obj):
     return twoMaxValue
 
 
-
 def H(Epb, Ep, E):
     if Ep / Epb == 1:
         return "rule"
@@ -19,12 +22,15 @@ def H(Epb, Ep, E):
 
 
 def lookForComplicatedRules(currentRecord, twoMaxValues):
-    newRule = {}
-    for attr, value in currentRecord.items():
-        for at, vl in twoMaxValues.items():
-            if attr == at:
-                newRule[attr] = value
-    print('---------------------')
     print("currentRecords: " + str(currentRecord))
     print("twoMaxValues: " + str(twoMaxValues))
+    newRule = {}
+    for attr, value in twoMaxValues.items():
+        newRule[attr] = currentRecord[attr]
     print("newRule: " + str(newRule))
+    print('---------------------')
+    # for x in data:
+    #     for attr, value in x.items():
+    #         if newRule[attr] is not None:
+    #             if x[attr] == newRule[attr]:
+    #                 print("record for new rule?: " + x)
